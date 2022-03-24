95dc5cc3812cc7d3a997a79e046efdc6f90414ae
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-23 21:23:34 -0400

    commit tree fd17ecdadb25cdaa7f779ef71f198b00b05c2adc

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+
+
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    let ignoreList = ['the', 'a', 'an', 'but', 'of', 'and', 'for' , 'at' , 'by', 'from']
+    let firstCap = Formatter.capitalize(string)
+    let stringArray = firstCap.split(' ')
+    let newArray = stringArray.map(x => ignoreList.includes(x) ? x : Formatter.capitalize(x))
+    return newArray.join(' ')
+  }
 }
